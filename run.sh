7686cee151406c366e0009b23b930a3c57f0b7f8
ZRHonor <zrhonour@outlook.com>
2020-12-05 12:37:42 +0800

    update

--- run.sh
+++ run.sh
@@ -52,7 +52,7 @@ dataset=cifar100
 loss_type=Seesaw_prior
 train_rule=None
 
-for imb_factor in 1 0.1 0.01
+for imb_factor in 0.01
 do
     python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
 done
@@ -62,7 +62,7 @@ dataset=cifar100
 loss_type=Seesaw
 train_rule=None
 
-for imb_factor in 1 0.1 0.01
+for imb_factor in 0.01
 do
     python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
 done

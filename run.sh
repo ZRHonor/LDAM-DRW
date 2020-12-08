0a1e8e622e42bad96e2b12fed0bbd9d9a789bfb3
ZRHonor <zrhonour@outlook.com>
2020-12-08 14:08:35 +0800

    debug

--- run.sh
+++ run.sh
@@ -1,61 +1,61 @@
 #!/bin/bash
 
 
-dataset=cifar100
-loss_type=CE
-train_rule=None
+# dataset=cifar100
+# loss_type=CE
+# train_rule=None
 
-for imb_factor in 1 10 100
-do
-    python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
-done
+# for imb_factor in 100 10 1
+# do
+#     python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
+# done
 
-dataset=cifar100
-loss_type=LDAM
-train_rule=None
+# dataset=cifar100
+# loss_type=LDAM
+# train_rule=None
 
-for imb_factor in 1 10 100
-do
-    python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
-done
-
-
-dataset=cifar100
-loss_type=CE
-train_rule=EffectiveNumber
-
-for imb_factor in 1 10 100
-do
-    python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
-done
+# for imb_factor in 100 10 1
+# do
+#     python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
+# done
 
 
-dataset=cifar100
-loss_type=CE
-train_rule=ClassBlance
+# dataset=cifar100
+# loss_type=CE
+# train_rule=EffectiveNumber
 
-for imb_factor in 1 10 100
-do
-    python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
-done
+# for imb_factor in 100 10 1
+# do
+#     python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
+# done
 
-dataset=cifar100
-loss_type=CE
-train_rule=ClassBlanceV2
 
-for imb_factor in 1 10 100
-do
-    python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
-done
+# dataset=cifar100
+# loss_type=CE
+# train_rule=ClassBlance
 
-dataset=cifar100
-loss_type=Focal
-train_rule=None
+# for imb_factor in 1 10 100
+# do
+#     python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
+# done
 
-for imb_factor in 1 10 100
-do
-    python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
-done
+# dataset=cifar100
+# loss_type=CE
+# train_rule=ClassBlanceV2
+
+# for imb_factor in 100 10 1
+# do
+#     python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
+# done
+
+# dataset=cifar100
+# loss_type=Focal
+# train_rule=None
+
+# for imb_factor in 100 10 1
+# do
+#     python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
+# done
 
 dataset=cifar100
 loss_type=SoftmaxGHMcV2
@@ -71,20 +71,20 @@ dataset=cifar100
 loss_type=SoftmaxGHMc
 train_rule=None
 
-for imb_factor in 1 10 100
+for imb_factor in 100 10 1
 do
     python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
 done
 
 
-dataset=cifar100
-loss_type=Seesaw_prior
-train_rule=None
+# dataset=cifar100
+# loss_type=Seesaw_prior
+# train_rule=None
 
-for imb_factor in 1 10 100
-do
-    python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
-done
+# for imb_factor in 100 10 1
+# do
+#     python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
+# done
 
 
 dataset=cifar100
@@ -124,3 +124,14 @@ for imb_factor in 100 10 1
 do
     python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type $loss_type --train_rule $train_rule
 done
+
+
+# dataset=cifar100
+# train_rule=None
+# imb_factor=100
+# for i in 1 12 123 1234 12345
+# do
+#     python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type SoftSeesaw --train_rule $train_rule --seed $i
+#     python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type Seesaw --train_rule $train_rule --seed $i
+#     python cifar_train.py --dataset $dataset --imb_factor $imb_factor --loss_type Seesaw_prior --train_rule $train_rule --seed $i
+# done
